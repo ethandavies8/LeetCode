47423254e56fb27debb3cff263a183b03fd256e3
ethandavies8 <ethandavies8@gmail.com>
2022-06-29 12:43:43 -0600

    Addes shuffle String #1528

---
+++ Solutions/RemoveLinkedListElements/HeapSortSolution.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KthLargestElementInArray
+{
+    class HeapSortSolution
+    {
+        public int FindKthLargest(int[] nums, int k)
+        {
+            HeapSort(nums, nums.Length);
+            return nums[nums.Length - k];
+        }
+
+        public static void HeapSort(int[] nums, int n)
+        {
+            for (int i = n / 2 - 1; i >= 0; i--)
+            {
+                Heapify(nums, n, i); //Fixes bottom half as well
+            }
+
+            for (int i = n - 1; i >= 0; i--)
+            { //Swap last and first indexes, then heapify
+                int temp = nums[0];
+                nums[0] = nums[i];
+                nums[i] = temp;
+                Heapify(nums, i, 0);
+            }
+        }
+
+        public static void Heapify(int[] nums, int n, int i)
+        { //Fixes heap structure
+            int largest = i, left = 2 * i + 1, right = 2 * i + 2;
+
+            //Find the largest child
+            if (left < n && nums[left] > nums[largest]) { largest = left; }
+            if (right < n && nums[right] > nums[largest]) { largest = right; }
+
+            //If root isn't largest, swap with whatever child is larger
+            if (largest != i)
+            {
+                int swap = nums[i];
+                nums[i] = nums[largest];
+                nums[largest] = swap;
+                Heapify(nums, n, largest); //Continue downwards
+            }
+        }
+    }
+}
---
+++ Solutions/ShuffleString1528/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShuffleString1528
+{
+    class Solution
+    {
+        //LeetCode Link: https://leetcode.com/problems/shuffle-string/
+
+        /// <summary>
+        /// You are given a string s and an integer array indices of the same length. The string s will be shuffled 
+        /// such that the character at the ith position moves to indices[i] in the shuffled string.
+        ///Return the shuffled string.
+        /// </summary>
+        /// <param name="s">string to be reordered based upon indices array</param>
+        /// <param name="indices">indices to change char locations</param>
+        /// <returns>string with char locations changed according to indices array</returns>
+        public static string RestoreString(string s, int[] indices)
+        {
+            StringBuilder output = new StringBuilder(s);
+            for(int i = 0; i < indices.Length; i++)
+            {
+                output[indices[i]] = s[i];
+            }
+            return output.ToString();
+
+        }
+
+        static void Main(string[] args)
+        {
+            string s = "codeleet";
+            string answer = "leetcode";
+            int[] indices = { 4, 5, 6, 7, 0, 2, 1, 3 };
+            string output = RestoreString(s, indices);
+            bool isTrue = answer.Equals(output);
+            Console.WriteLine(output);
+        }
+    }
+}
